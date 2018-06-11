2b466ad2a27f9d6a6ba252cf29fa46ac97d90bcc
watye <watye@qq.com>
2018-06-11 16:21:56 +0800

    添加swagger

--- src/main/java/com/talelife/DemoApplication.java
+++ src/main/java/com/talelife/DemoApplication.java
@@ -4,8 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @MapperScan("com.talelife.myproject.mapper")
+@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) throws Exception {
